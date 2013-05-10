14df22be0bbd0fe0ba88cb8cfecc86feb4fc68ae
Daniel Brown <ddb@star.sr.bham.ac.uk>
2013-05-09 17:32:43 -0700

    adding parser.py file that doesn't really do anything yet

---
+++ pykat/parser.py
@@ -0,0 +1,36 @@
+import os
+import exceptions
+import numpy as np
+
+from pykat.node_network import NodeNetwork
+from pykat.detectors import Detector
+from pykat.components import Component
+from pykat.commands import Command, xaxis
+
+components = np.array(['m','m1','m2','l','s','bs','bs1','bs2','pd','pd*'])
+commands = np.array(['attr','tem','tem*','gauss','gauss*','gauss**','cav','conf'])
+
+
+# some commands we ignore, we do the plotting with pyhton
+# so don't need 
+ignore = ['gnuterm']
+def parse_kat_file(kat_filename):
+    
+    kat_cmps = [] # holds the components found in kat file
+    kat_cmds = [] # holds the commands found in kat file
+    
+    katfile = open(kat_filename,'r')
+    
+    for line in katfile.readlines():
+        arg = line.split(' ')[0]
+        
+        # c
+        if (components == arg).any():
+            kat_cmps.append()
+            
+        elif (commands == arg).any():
+            
+        
+def parse_m(line):
+    return line
+    
