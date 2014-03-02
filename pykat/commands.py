651dbe0dfbc1b8f0d457af4eaed9144f84ae09f5
Andreas Freise <adf@star.bham.ac.uk>
2014-03-02 17:39:41 +0000

    fixing parsing error for gauss commands.

--- pykat/commands.py
+++ pykat/commands.py
@@ -46,16 +46,13 @@ class gauss(object):
     @staticmethod
     def parseFinesseText(text, kat):
         values = text.split()
-
-        if not values[0].startswith("gauss") or (len(values) != 6 and len(values != 8)):
+        if not values[0].startswith("gauss") or (len(values) != 6 and len(values) != 8):
             raise exceptions.RuntimeError("'{0}' not a valid Finesse gauss command".format(text))        
         
         name = values[1]
         component = values[2]
         node = values[3]
-        
-        
-        
+                    
         if values[0].endswith("**"):
             if len(values) == 6:
                 print ""
