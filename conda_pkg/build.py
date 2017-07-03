7f488043b72a328ce8c6f5e9f57df5a0affc6104
Daniel Brown <ddb@star.sr.bham.ac.uk>
2017-07-03 23:49:15 +0100

    updating build

--- conda_pkg/build.py
+++ conda_pkg/build.py
@@ -2,10 +2,10 @@ import os
 import os.path
 import subprocess
 
-os.environ['CONDA_BUILD_VERSION'] = 'master'
+
 
 subprocess.check_call(["conda", "config", "--add", "channels", "gwoptics"])
-
-for _ in ["2.7", "3.4", "3.5", "3.6"]:
-    subprocess.check_call(["conda", "build", "purge"])
-    subprocess.check_call(["conda", "build", "--py", _, "pykat"])
+subprocess.check_call(["conda", "build", "purge"])
+subprocess.check_call(["conda", "build", "--output-folder", "packages", "pykat"])
+    
+    
