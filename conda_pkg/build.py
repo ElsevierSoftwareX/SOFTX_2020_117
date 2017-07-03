02cb82b918f4d0ff5fedf42b43aa0c7318e03316
Daniel Brown <ddb@star.sr.bham.ac.uk>
2017-07-04 00:52:25 +0100

    more build updates

--- conda_pkg/build.py
+++ conda_pkg/build.py
@@ -1,6 +1,14 @@
 import os
 import os.path
 import subprocess
+import pykat
+
+import subprocess
+
+version_git = subprocess.check_output(["git", "describe","--long"]).decode('utf8').rstrip()
+version_git = ".".join(version_git.split('-')[:2])
+    
+os.environ['CONDA_PYKAT_VERSION'] = version_git
 
 subprocess.check_call(["conda", "config", "--add", "channels", "gwoptics"])
 subprocess.check_call(["conda", "build", "purge"])
