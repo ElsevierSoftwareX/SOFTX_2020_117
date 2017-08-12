709ab8306a2efdd3facaf5ab6f97530737d3a3ae
Daniel Brown <ddb@star.sr.bham.ac.uk>
2017-08-12 12:15:57 +0100

    adding mising file

---
+++ pykat/pipe_comm.py
@@ -0,0 +1,66 @@
+import time
+import sys
+import struct
+
+def open_pipes(pipe_name, start_kat, duration):
+    fifo_r = None
+    fifo_w = None
+    
+    while fifo_r is None or fifo_w is None:
+        try:
+            if time.time() < start_kat + duration:
+                if fifo_w is None:
+                    fifo_w = open(pipe_name+".pf", "wb", 0)
+                
+                if fifo_r is None:
+                    fifo_r = open(pipe_name+".fp", "rb", 0)
+            else:
+                raise pkex.BasePyKatException("Could not connect to pykat pipe in {0} seconds. Ensure you are using Finesse >= v2.1 and Pykat >= v1.0.0. Or set usePipe=False when making kat object.".format(duration))
+                
+        except FileNotFoundError as ex:
+            sys.stdout.flush()
+            
+    return fifo_r, fifo_w
+                    
+def send_test(fifo_w, a, b, c):
+    if fifo_w is not None:
+        data = [a,b,c]
+        bdata = struct.pack('ddi', *data)
+        
+        fifo_w.write(("<test>").encode())
+        fifo_w.write(struct.pack('b', len(bdata)))
+        fifo_w.write(bdata)
+        fifo_w.flush()
+        print("Sent",len(bdata), "bytes")
+        
+def send_test_str(fifo_w, a):
+    if fifo_w is not None:
+        bdata = struct.pack('20s', a.encode('ascii'))
+        
+        fifo_w.write(("<test_str>").encode())
+        fifo_w.write(struct.pack('b', len(bdata)))
+        fifo_w.write(bdata)
+        fifo_w.flush()
+        print("Sent",len(bdata), "bytes")
+        
+def send_finished(fifo_w):
+    if fifo_w is not None:
+        fifo_w.write(("<finished>").encode())
+        fifo_w.write(struct.pack('b', 0))
+        fifo_w.flush()
+        
+def send_do_step(fifo_w):
+    if fifo_w is not None:
+        fifo_w.write(("<do_step>").encode())
+        fifo_w.write(struct.pack('b', 0))
+        fifo_w.flush()
+        
+def send_update(fifo_w, idx, value):
+    if fifo_w is not None:
+        bdata = struct.pack('id', idx, value)
+        
+        fifo_w.write(("<update>").encode())
+        fifo_w.write(struct.pack('b', struct.calcsize('i') + struct.calcsize('d')))
+        fifo_w.write(struct.pack('i', idx))
+        fifo_w.write(struct.pack('d', value))
+        fifo_w.flush()
