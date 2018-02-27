ca5272c218c8f932916d0a342d875b7b8406b9dd
Daniel Brown <daniel.d.brown@adelaide.edu.au>
2018-02-27 17:04:37 +1030

    updating knm test case

--- examples/knm.py
+++ examples/knm.py
@@ -1,12 +1,24 @@
 import pykat
-from pykat.optics.knm import *
+from pykat.optics.knm import plot_knm_matrix, knmHG, makeCouplingMatrix
 from pykat.optics.maps import *
 import numpy as np
 
-
-C = makeCouplingMatrix(10)
-
 q1 = pykat.BeamParam(w0=5e-2, z=0)
+q2 = pykat.BeamParam(w0=5e-2, z=0)
 
-K = knmHG(C, q1, q1, surface_map=m)
+R  = max(q1.w, q2.w)
+
+N  = 500
+dx = R*6/N
+
+m = surfacemap("empty",
+               "phase both",
+               size=(N, N),
+               center=((N+1)/2, (N+1)/2),
+               step_size=(dx, dx))
+
+C = makeCouplingMatrix(2)
+
+K = knmHG(C, q1, q2, surface_map=m)
+
 plot_knm_matrix(C, abs(K))
