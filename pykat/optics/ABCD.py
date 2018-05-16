Fixes to beamsize method for BeamParam, using wrong sign BeamParam passes wavelength now when doing operations Fixing fsig -> _fsig class name change

pykat/optics/ABCD.py
@@ -11,9 +11,9 @@ def apply(ABCD, q1, n1, n2):
     n2: output refractive index
     """
     # make sure we have a complex here. We could have a beamParam object or a an actual complex value supplied
-    q1 = complex(q1)
+    #_q1 = complex(q1)
         
-    return BeamParam(nr=n2, q=n2 * (ABCD[0,0] * q1/float(n1) + ABCD[0,1]) / (ABCD[1,0] * q1/float(n1) + ABCD[1,1]))
+    return BeamParam(nr=n2, q=n2 * (ABCD[0,0] * q1/float(n1) + ABCD[0,1]) / (ABCD[1,0] * q1/float(n1) + ABCD[1,1]), wavelength=q1.wavelength)
 
 def mirror_trans(n1, n2, Rc):
     return np.matrix([[1.0,0.0],[(n2-n1)/float(Rc),1.0]])

pykat/optics/gaussian_beams.py
@@ -72,6 +72,9 @@ class BeamParam(object):
     @wavelength.setter
     def wavelength(self,value): self.__lambda = SIfloat(value)
     
+    def __repr__(self):
+        return "<%s (w0=%s, w=%s, z=%s) at %s>" % (self.__class__.__name__, self.w0, self.w, self.z, hex(id(self)))
+        
     @property
     def nr(self): return self.__nr
     
@@ -100,7 +103,7 @@ class BeamParam(object):
         if z is None:
             z = self.z
         else:
-            z = np.array(z-self.z)
+            z = np.array(z+self.z)
                 
         if wavelength is None:
             wavelength = self.wavelength
@@ -310,6 +313,9 @@ class BeamParam(object):
         return BeamParam(self.__lambda, self.__nr, complex(a) - self.__q)
     
     def __div__(self, a):
+        return BeamParam(self.__lambda, self.__nr, self.__q / complex(a))
+        
+    def __truediv__(self, a):
         return BeamParam(self.__lambda, self.__nr, self.__q / complex(a))
     
     def __idiv__(self, a):