ElsevierSoftwareX/SOFTX_2020_117

commit 833b137243c522dd234f63dd62eef88f4f5efd7d

Fix bug where trailing percent-style comments were not removed before trying to parse commands

pykat/finesse.py
@@ -3258,7 +3258,7 @@ class kat(object):
                 # remove any mutliple whitespace
                 line = " ".join(line.split())
                 # add to a list all the positions of any inline comment markers
-                i = [line.find('#'), line.find('\\')]
+                i = [line.find('#'), line.find('%'), line.find('\\')]
                 #i = filter(lambda a: a != -1, i)
                 i = [a for a in i if a != -1]
 

commit 5c58cf2981f7182d7a3fe8652ff15057e31817fb

Previous commit 833b1372 broke block parsing, as they rely on % comments signs which got removed, adding whitespace check for end of line comments

pykat/finesse.py
@@ -3306,7 +3306,7 @@ class kat(object):
                 # remove any mutliple whitespace
                 line = " ".join(line.split())
                 # add to a list all the positions of any inline comment markers
-                i = [line.find('#'), line.find('%'), line.find('\\')]
+                i = [line.find('#'), line.find(' %'), line.find('\\')]
                 #i = filter(lambda a: a != -1, i)
                 i = [a for a in i if a != -1]
 